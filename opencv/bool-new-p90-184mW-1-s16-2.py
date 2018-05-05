879bd02d3db0ce3852f9a730eaf986f93ee40083
Tairy <tairyguo@gmail.com>
2018-05-05 14:18:07 +0800

    增加计算脚本

---
+++ opencv/bool-new-p90-184mW-1-s16-2.py
@@ -0,0 +1,72 @@
+# encoding:utf-8
+import os
+import cv2
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+cap = cv2.VideoCapture('./p0-54.2mW-1-s16.wmv')
+
+feature_params = dict( maxCorners = 100,
+                       qualityLevel = 0.3,
+                       minDistance = 7,
+                       blockSize = 7)
+
+
+width = 30
+height = 30
+arr = np.zeros((height, width))
+index = 0
+times = 0
+
+while True:
+  ret, frame = cap.read()
+  if ret == False:
+    break
+
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  gray = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+  center = gray[600:630, 805:835]
+  origin_center = frame[600:630, 805:835]
+
+  im2, contours, hierarchy = cv2.findContours(center, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  if len(contours) >= 2:
+    # cnt = max(contours, key = cv2.contourArea)
+    cnt = contours[1]
+    M = cv2.moments(cnt)
+    if M['m00'] > 0:
+      cx = int(M['m10']/M['m00'])
+      cy = int(M['m01']/M['m00'])
+      cv2.circle(origin_center,(cx,cy), 1, (0,255,0), 1)
+    # print(cx, cy)
+      arr[cx, cy] += 1
+    # x,y,w,h = cv2.boundingRect(cnt)
+    # # cv2.rectangle(frame,(795 + x, 600 + y),(795 + x + w, 600 + y + h),(0,255,0),1)
+    # cv2.rectangle(origin_center,(x, y),(x + w, y + h),(0,255,0),1)
+    # _center = (x + w) / 2, (y + h) / 2
+    # a_x = int(((x + w) / 2))
+    # a_y = int(((y + h) / 2))
+    # arr[a_x, a_y] += 1
+    # print(_center)
+    # cv2.drawContours(origin_center, contours, 1, (0, 0, 255), 1)
+    # for cnt in contours:
+    #   x,y,w,h = cv2.boundingRect(cnt)
+    #   cv2.rectangle(origin_center,(x,y),(x+w,y+h),(0,255,0),1)
+    # (x,y),radius = cv2.minEnclosingCircle(cnt)
+    # _center = (int(x),int(y))
+
+    # print(_center)
+    # radius = int(radius)
+    # print(radius)
+    # cv2.circle(origin_center,_center,radius,(0,255,0),1)
+
+#   cv2.imshow("capture", origin_center)
+#   if cv2.waitKey(1) & 0xFF == ord('q'):
+#     break
+# cap.release()
+cv2.destroyAllWindows()
+cap.release()
+plt.matshow(arr, cmap='hot')
+plt.colorbar()
+plt.show()  
--- opencv/bool.py
+++ opencv/bool.py
@@ -4,21 +4,15 @@ import cv2
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 
 '''
 识别视频中运动的点
 '''
 
-cap = cv2.VideoCapture('./3.avi')
-feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.3,
-                       minDistance = 7,
-                       blockSize = 7)
+cap = cv2.VideoCapture('./42.8mW-P0-single particle_0.avi')
 
-width = 30
-height = 30
+width = 50
+height = 50
 arr = np.zeros((height, width))
 index = 0
 times = 0
@@ -26,95 +20,37 @@ while True:
   ret, frame = cap.read()
   if ret == False:
     break
-    # index = 0
-    # times += 1
-    # if times >= 10:
-      # break
-    # cap = cv2.VideoCapture('./3.avi')
-    # continue
   gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  gray = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-
-  # 2.avi
-  # center = gray[90:120, 100:130]
-  # origin_center = frame[90:120, 100:130]
-  
-  # 3.avi
-  center = gray[70:130, 80:140]
-  origin_center = frame[70:130, 80:140]
-
-
+  gray = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+  center = gray[70:120, 85:145]
+  origin_center = frame[70:120, 85:135]
   index += 1
-  # if index > 1200:
-    # continue
-#   print(index)
-  if index > 1080 or index < 530:
+  if index >= 1300:
     continue
-#   cv2.imshow("capture", center)
-#   if cv2.waitKey(1) & 0xFF == ord('q'):
-#     break
-# cap.release()
-# cv2.destroyAllWindows()
-
   im2, contours, hierarchy = cv2.findContours(center, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
   if len(contours) != 0:
-    # draw in blue the contours that were founded
-
     #find the biggest area
     cnt = max(contours, key = cv2.contourArea)
-
-    # cv2.drawContours(origin_center, [c], -1, (0, 0, 255), 2)
-    # cv2.drawContours(origin_center, contours, -1, (0, 0, 255), 2)
-    # ellipse = cv2.fitEllipse(cnt)
-    # cv2.ellipse(origin_center,ellipse,(0,255,0))
-    # rect = cv2.minAreaRect(cnt)
-    # box = cv2.boxPoints(rect)
-    # box = np.int0(box)
-    # cv2.drawContours(origin_center,[box],0,(0, 0, 255), 2)
-    (x,y),radius = cv2.minEnclosingCircle(cnt)
-    _center = (int(x),int(y))
-    print(_center)
-    # for _x in range(-1, 1):
-    #   for _y in range(-1, 1):
-    #     arr[int(y) + _y, int(x) + _x] += 1
-    # if int(y) in range(5, 20) or int(x) in range(5, 25):
-      # arr[int(y), int(x)] += 1
-
-    # print(center)
-    radius = int(radius)
-    cv2.circle(origin_center,_center,radius,(0,255,0),1)
-
-
-#     # x,y,w,h = cv2.boundingRect(c)
-#     # draw the book contour (in green)
-#     # cv2.rectangle(origin_center,(x,y),(x+w,y+h),(0,255,0),2)
-#   # print(contours)
-#   # print(len(contours))
-#   # cv2.drawContours(origin_center, contours, -1, (0, 0, 255), 2)
-
-#   # p0 = cv2.goodFeaturesToTrack(center, mask = None, **feature_params)
-#   # if p0 is not None:
-#   #   x_sum = 0
-#   #   y_sum = 0
-#   #   for i, old in enumerate(p0):
-#   #     x, y = old.ravel()
-#   #     x_sum += x
-#   #     y_sum += y
-#   #   x_avg = int(x_sum / len(p0))
-#   #   y_avg = int(y_sum / len(p0))
-#   #   for x in range(-2, 2):
-#   #     for y in range(-2, 2):
-#   #       arr[y_avg + y, x_avg + x] += 1
-#     # arr[y_avg, x_avg] += 1
-#     # cv2.circle(origin_center,(x_avg,y_avg),2,(0,255,0),2)
-  cv2.imshow("capture", origin_center)
+    cv2.drawContours(im,contours,-1,(0,255,0),3)
+    # cv2.drw
+    # M = cv2.moments(cnt)
+    # if M['m00'] > 0:
+    #   cx = int(M['m10']/M['m00'])
+    #   cy = int(M['m01']/M['m00'])
+    #   #print(cx, cy)
+    #   cv2.circle(origin_center,(cx,cy), 1, (0,0,255), 1)
+    # # print(cx, cy)
+    #   arr[cx, cy] += 1
+  # # movie
+  cv2.imshow("capture", center)
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
 cap.release()
 cv2.destroyAllWindows()
 
-# arr = arr[20:50, 15:45]
+#--- draw hot image
+# cv2.destroyAllWindows()
 # cap.release()
 # plt.matshow(arr, cmap='hot')
 # plt.colorbar()
-# plt.show()
+# plt.show() 
